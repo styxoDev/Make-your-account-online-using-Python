6661624047255849cfb646c24126d68dd63bb2c4
Styxo <96294167+styxoDev@users.noreply.github.com>
2022-01-06 17:57:09 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+    return '<meta http-equiv="refresh" content="0; URL=https://replit.com"/>'
+
+def run():
+    app.run(host="0.0.0.0", port=8000)
+
+def server_start():
+    server = Thread(target=run)
+    server.start()
